338dcd5102cf958014f898693c6d05a3fc4138d8
zsevic <zsevic95@gmail.com>
2022-11-28 13:54:39 +0100

    Remove unused code

--- src/modules/chatbot/chatbot.module.ts
+++ src/modules/chatbot/chatbot.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from 'modules/user/user.module';
 import { ChatbotService, MessageService, ResponseService } from './services';
 
 @Module({
-  imports: [UserModule],
   providers: [ChatbotService, MessageService, ResponseService],
 })
 export class ChatbotModule {}
--- src/modules/user/create-user.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateUserDto {
-  public chatId: string;
-}
